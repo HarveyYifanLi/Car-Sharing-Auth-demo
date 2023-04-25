a181b8a2ae2357f982e0746a81de255f0bc1f8cb
Yifan Li <yifanli@Yifans-MacBook-Pro.local>
2023-04-25 16:31:53 -0700

    connect server to MongoDB Atlas

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config(); // load all the env variables to the process.env by using dotenv package
+
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/modo-cars-db"
+  process.env.MONGODB_ATLAS_URI || "mongodb://localhost/modo-cars-db"
 );
 
 module.exports.User = require("./user");
