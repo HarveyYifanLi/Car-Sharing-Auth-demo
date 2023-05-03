33b9bb28cf628ed4e86e84a644018565d645c63c
Yifan Li <yifanli@Yifans-MacBook-Pro.local>
2023-05-02 18:54:23 -0700

    Use Kubernetes Service entry point listening at port 80 created for redis, as the Redis client's url

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const redisClient = redis.createClient({
   legacyMode: true,
   // PORT: REDIS_PORT // use this locally
-  url: 'redis://my-redis-container:6379', // i.e. 'my-redis-container' is the name of the Redis (client) Container created and running in the Docker network
+  // url: 'redis://my-redis-container:6379', // i.e. 'my-redis-container' is the name of the Redis (client) Container created and running in the Docker network
+  url: 'redis://redis-service:80', // i.e. 'redis-service' is the kubernetes Service resource/entry-point (which gets requests at port:80) created for redis Deployment resource
 });
 
 (async () => { 
